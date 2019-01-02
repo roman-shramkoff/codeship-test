f61a5b1e8e8236a97ca2999467fba0d31586cb3f
Roman_Shramkov <roman_shramkov@epam.com>
2019-01-02 12:05:51 +0200

    Added test Spring Booth app

---
+++ src/main/java/org/romans/blueoceantest/BlueoceanTestApplication.java
@@ -0,0 +1,12 @@
+package org.romans.blueoceantest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueoceanTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueoceanTestApplication.class, args);
+	}
+}
---
+++ src/test/java/org/romans/blueoceantest/BlueoceanTestApplicationTests.java
@@ -0,0 +1,20 @@
+package org.romans.blueoceantest;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlueoceanTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	    assertTrue("It is true", true);
+	}
+
+}
